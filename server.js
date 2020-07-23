70525ed53565d866666f7f6421b96bd959864f74
da-the-dev <leshiktk@me.com>
2020-07-23 20:20:43 +0300

    Everything works on host device

--- server.js
+++ server.js
@@ -1,17 +1,38 @@
+const express = require('express')
+const app = express()
+const server = require('http').createServer(app)
 const WebSocket = require('ws')
 
-const server = new WebSocket.Server({ port: 3000 });
+let wss = new WebSocket.Server({ server: server })
 
-server.once('listening', () => {
-    console.log("WebSocket server is now listening");
+var temp = 22
+
+/**
+ * 
+ * @param {WebSocket.Server} wss 
+ */
+var sendTemp = (wss) => {
+    temp++;
+    wss.clients.forEach(c => {
+        c.send(`$temp ${temp}`)
+    })
+}
+
+wss.on('connection', (ws) => {
+    console.log('[Server] New client connected');
+
+    ws.on('message', (message) => {
+        if(message === "close") {
+            wss.clients.forEach(c => {
+                c.close()
+            })
+            wss.close()
+        }
+    })
+
+    setInterval(sendTemp, 1000, wss)
 })
 
-server.on('connection', ws => {
-    console.log('Someone connected')
-    ws.send('Welcome');
+app.get('/', (req, res) => { })
 
-    ws.on('message', msg => {
-        console.log(`Message recieved: ${msg}`);
-    });
-
-});
+server.listen(3000, () => console.log("[Server] Listening on port: 3000"))
