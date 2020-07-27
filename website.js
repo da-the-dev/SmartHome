b2d2b12cd767b79879d78dab4b87848a67f22243
da-the-dev <leshiktk@me.com>
2020-07-27 22:59:02 +0300

    Cleaned up + added auto-detect ip functionality

--- website.js
+++ website.js
@@ -1,13 +1,19 @@
-const express = require('express')
+const toolkit = require('./toolkit')
+const express = require('express');
 const app = express()
 const server = require('http').Server(app)
-const host = '192.168.2.218'
+
+const host = toolkit.findHost();
 const port = 3000
 
 app.use(express.static(process.cwd()))
 
 app.get('/', (req, res) => {
     res.sendFile(process.cwd() + '/index.html')
+})
+
+app.get('/arduino_check', (req, res) => {
+    res.send("responce from website")
 })
 
 server.listen(port, host, () => {
