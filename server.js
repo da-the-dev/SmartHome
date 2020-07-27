b76371160f61268964b60550ac7410568bad0ad0
da-the-dev <leshiktk@me.com>
2020-07-27 22:58:32 +0300

    Cleaned up + added auto-detect ip functionality

--- server.js
+++ server.js
@@ -1,29 +1,17 @@
+const toolkit = require('./toolkit')
 const express = require('express')
 const app = express()
 const server = require('http').createServer(app)
 const WebSocket = require('ws')
 
-const host = "192.168.2.218"
+// Find IP
+const host = toolkit.findHost()
 const port = 8080
 
+// Start the server
 let wss = new WebSocket.Server({ server: server })
 
-var temp = 22
-
-/**
- * 
- * @param {WebSocket.Server} wss 
- */
-var sendTemp = (wss) => {
-    temp++;
-    wss.clients.forEach(c => {
-        c.send(`$temp ${temp}`)
-    })
-}
-
 wss.on('connection', (ws) => {
-    console.log('[Server] New client connected');
-
     ws.on('message', (message) => {
         if(message === "close") {
             wss.clients.forEach(c => {
@@ -32,18 +20,15 @@ wss.on('connection', (ws) => {
             wss.close()
         }
 
-        if(message.startsWith("$temp")) {
-            var data = parseInt(message.substring(5))
-            wss.clients.forEach(c => {
-                if(c != ws && ws.readyState == WebSocket.OPEN)
-                    c.send(`$website ${data}`)
-            })
-        }
+        wss.clients.forEach(c => {
+            if(c != ws && ws.readyState == WebSocket.OPEN)
+                c.send(message)
+        })
 
-        console.log("[Server] Message: " + message);
+        if(message === "ALC")
+            console.log("[Server] Arduino Light Connected!")
     })
 })
 
 app.get('/', (req, res) => { })
-
-server.listen(port, host, () => console.log(`[Server] Listening on ${host}/${port}/`))
+server.listen(port, host, () => console.log(`[Server] Listening on ${host}:${port}`))
