fbd1512f131ec021286fff52b2a290824e4aea73
da-the-dev <leshiktk@me.com>
2020-08-10 16:13:24 +0300

    Status updates

--- server.js
+++ server.js
@@ -8,26 +8,65 @@ const WebSocket = require('ws')
 const host = toolkit.findHost()
 const port = 8080
 
+// Clients and arduinos 
+/**@type {Array<WebSocket>} */
+var clients = new Array()
+/**@type {Map<WebSocket, any>} */
+var arduinos = new Map()
+
 // Start the server
 let wss = new WebSocket.Server({ server: server })
 
 wss.on('connection', (ws) => {
-    ws.on('message', (message) => {
-        if(message === "close") {
-            wss.clients.forEach(c => {
-                c.close()
-            })
-            wss.close()
+    ws.on('message', msg => {
+        // Client connected. End here, no msg is transmitted
+        if(msg === "%") {
+            console.log(`[Server] Client connected! Client count: ${wss.clients.size}`)
+            clients.push(ws)
+            return
         }
 
-        wss.clients.forEach(c => {
-            if(c != ws && ws.readyState == WebSocket.OPEN)
-                c.send(message)
+        // Message broadcast
+        if(clients.includes(ws)) // If client sent a msg
+            arduinos.forEach(a => { // Send it to all arduinos
+                a.self.send(msg)
+            })
+
+
+        clients.forEach(c => { // Send it to all clients
+            c.send(msg)
         })
 
-        if(message === "ALC")
-            console.log("[Server] Arduino Light Connected!");
+
+        // Arduino connected
+        if(!arduinos.has(ws)) {
+            console.log(`[Server] Arduino connected! Client count: ${wss.clients.size}`)
+            arduinos.set(ws, {
+                "self": ws,
+                "connected": true,
+                "status": "none",
+                "value": "none"
+            })
+            return
+        }
+
+        /**@type {Array<string>} */
+        const args = msg.split(" ")
+        const name = args.shift()
+
+        var data = arduinos.get(ws)
+        if(!isNaN(args[0])) { // If recieved a number, it's a value
+            data.value = args[0]
+            arduinos.set(ws, data)
+        } else if(args[0]) { // If recieved a string, it's a status
+            data.status = args[0]
+            arduinos.set(ws, data)
+        }
     })
+
+    ws.onclose = () => {
+        console.log(`[Server] Someone disconnected! Client count: ${wss.clients.size}`)
+    }
 })
 
 app.get('/', (req, res) => { })
